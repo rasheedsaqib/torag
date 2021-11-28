87b8cf25fb389736a84dd80592fc8f482b9c029b
unknown <mrasheed.bese19seecs@seecs.edu.pk>
2021-11-29 00:53:29 +0500

    V1

---
+++ assets/js/contact-form-script.js
@@ -0,0 +1,42 @@
+/*==============================================================*/
+// Ferry Contact Form  JS
+/*==============================================================*/
+(function ($) {
+    "use strict"; // Start of use strict
+    $("#contactForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            formError();
+            submitMSG(false, "Did you fill in the form properly?");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            submitForm();
+        }
+    });
+
+
+    function submitForm(){
+        $(".alert").css("display", "block");
+    }
+
+    function formSuccess(){
+        $("#contactForm")[0].reset();
+        submitMSG(true, "Message Submitted!")
+    }
+
+    function formError(){
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).removeClass();
+        });
+    }
+
+    function submitMSG(valid, msg){
+        if(valid){
+            var msgClasses = "h4 tada animated text-success";
+        } else {
+            var msgClasses = "h4 text-danger";
+        }
+        $("#msgSubmit").removeClass().addClass(msgClasses).text('');
+    }
+}(jQuery)); // End of use strict
